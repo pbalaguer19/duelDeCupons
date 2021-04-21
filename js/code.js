e2a42f0e9d53ae3d199da0a3d67dcf270288958a
Pau Balaguer Llaquet <pbalaguer@MacBook-Air-de-Pau-2.local>
2021-04-21 19:08:41 +0200

    Working...

--- js/code.js
+++ js/code.js
@@ -1,2 +1,16 @@
-var elementNum = parseInt(Math.random() * 7249);
-var product = data[elementNum]
+var elementNum = parseInt(Math.random() * data.length);
+var product = data[elementNum];
+
+document.getElementById("img").src = product["img"];
+document.getElementById("product").innerHTML = product["name"];
+document.getElementById("price").innerHTML = product["price"] + "€";
+document.getElementById("url").href = product["url"];
+
+updateRange();
+
+function updateRange(){
+    var range = parseInt(document.getElementById("range").value);
+    document.getElementById("opcio1").innerHTML = range + "%";
+    document.getElementById("opcio2").innerHTML = range + "€";
+}
+
