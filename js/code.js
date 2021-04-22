7cb7f3c4ccda8174b177e997d20eae7a52c7462e
Pau Balaguer Llaquet <pbalaguer@MacBook-Air-de-Pau-2.local>
2021-04-22 08:39:15 +0200

    Basic functionality working

--- js/code.js
+++ js/code.js
@@ -1,5 +1,7 @@
 var elementNum = parseInt(Math.random() * data.length);
 var product = data[elementNum];
+var finalPriceEuros;
+var finalPricePercent;
 
 document.getElementById("img").src = product["img"];
 document.getElementById("product").innerHTML = product["name"];
@@ -12,5 +14,23 @@ function updateRange(){
     var range = parseInt(document.getElementById("range").value);
     document.getElementById("opcio1").innerHTML = range + "%";
     document.getElementById("opcio2").innerHTML = range + "€";
+
+    finalPriceEuros = product["price"] - parseInt(range);
+    finalPricePercent = product["price"] - (product["price"] * parseInt(range) / 100);
+
+    if (finalPriceEuros < 0) finalPriceEuros = 0;
+    if (finalPricePercent < 0) finalPricePercent = 0;
+}
+
+function euros(){
+    if (finalPriceEuros < finalPricePercent) alert("Has escollit l'opció més barata!");
+    if (finalPriceEuros == finalPricePercent) alert("Les dues opcions tenen el mateix preu final!");
+    if (finalPriceEuros > finalPricePercent) alert("Ep! Has escollit l'opció més cara!");
+}
+
+function percent(){
+    if (finalPriceEuros > finalPricePercent) alert("Has escollit l'opció més barata!");
+    if (finalPriceEuros == finalPricePercent) alert("Les dues opcions tenen el mateix preu final!");
+    if (finalPriceEuros < finalPricePercent) alert("Ep! Has escollit l'opció més cara!");
 }
 
