10ea62501a3e8c9542e57a1d5d260d4e00c629c4
Pau Balaguer Llaquet <pbalaguer@MacBook-Air-de-Pau-2.local>
2021-04-23 09:45:47 +0200

    Basic functionality working

--- js/code.js
+++ js/code.js
@@ -1,14 +1,21 @@
-var elementNum = parseInt(Math.random() * data.length);
-var product = data[elementNum];
+var elementNum;
+var product;
 var finalPriceEuros;
 var finalPricePercent;
 
-document.getElementById("img").src = product["img"];
-document.getElementById("product").innerHTML = product["name"];
-document.getElementById("price").innerHTML = product["price"] + "€";
-document.getElementById("url").href = product["url"];
+getRandomElement();
 
-updateRange();
+function getRandomElement(){
+    elementNum = parseInt(Math.random() * data.length);
+    product = data[elementNum];
+
+    document.getElementById("img").src = product["img"];
+    document.getElementById("product").innerHTML = product["name"];
+    document.getElementById("price").innerHTML = product["price"] + "€";
+    document.getElementById("url").href = product["url"];
+
+    updateRange();
+}
 
 function updateRange(){
     var range = parseInt(document.getElementById("range").value);
@@ -23,14 +30,29 @@ function updateRange(){
 }
 
 function euros(){
-    if (finalPriceEuros < finalPricePercent) alert("Has escollit l'opció més barata!");
-    if (finalPriceEuros == finalPricePercent) alert("Les dues opcions tenen el mateix preu final!");
-    if (finalPriceEuros > finalPricePercent) alert("Ep! Has escollit l'opció més cara!");
+
+    if (finalPriceEuros < finalPricePercent) cheapestOption(finalPriceEuros);
+    if (finalPriceEuros == finalPricePercent) equalOption(finalPriceEuros);
+    if (finalPriceEuros > finalPricePercent) equalOption(finalPriceEuros, finalPricePercent);
 }
 
 function percent(){
-    if (finalPriceEuros > finalPricePercent) alert("Has escollit l'opció més barata!");
-    if (finalPriceEuros == finalPricePercent) alert("Les dues opcions tenen el mateix preu final!");
-    if (finalPriceEuros < finalPricePercent) alert("Ep! Has escollit l'opció més cara!");
+    if (finalPriceEuros > finalPricePercent) cheapestOption(finalPricePercent);
+    if (finalPriceEuros == finalPricePercent) equalOption(finalPricePercent);
+    if (finalPriceEuros < finalPricePercent) mostExpensiveOption(finalPricePercent, finalPriceEuros);
 }
 
+function cheapestOption(price){
+    document.getElementById("exampleModalLabel").innerHTML = "Has escollit l'opció més barata!"
+    document.getElementById("modalBody").innerHTML = "El preu final són " + (Math.round(price * 100) / 100) + "€"
+}
+
+function equalOption(price){
+    document.getElementById("exampleModalLabel").innerHTML = "Les dues opcions tenen el mateix preu final!"
+    document.getElementById("modalBody").innerHTML = "El preu final són " + (Math.round(price * 100) / 100) + "€"
+}
+
+function mostExpensiveOption(price, cheapest){
+    document.getElementById("exampleModalLabel").innerHTML = "Has escollit l'opció més cara!"
+    document.getElementById("modalBody").innerHTML = "El preu final són " + (Math.round(price * 100) / 100) + "€ i amb l'altre descompte el preu final era " + cheapest + "€."
+}
